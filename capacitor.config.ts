dfb592bce7a5f75021eba2e9b7ed0f2087817727
Sean Perkins <sean@ionic.io>
2023-04-06 13:07:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-reminders-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
